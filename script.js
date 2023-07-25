abc8283d017f7801348711de57e936b725fd6af6
sarperxx <43006015+sarperxx@users.noreply.github.com>
2023-07-25 12:52:51 +0300

    trying to solve

---
+++ script.js
@@ -0,0 +1,31 @@
+// Sets important constants and variables
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.querySelectorAll(".cell");
+
+//Default grid
+
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+  // Creates rows
+  for (r = 0; r < rowNum; r++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "gridRow";
+  }
+}
+
+// Creates columns
+function makeColumns(cellNum) {
+  for (i = 0; i < rows.length; i++) {
+    for (j = 0; j < cellNum; j++) {
+      let newCell = document.createElement("div");
+      rows[j].appendChild(newCell).className = "cell";
+    }
+  }
+}
+makeRows(16);
+makeColumns(16);
+
+addEventListener("mouseover", (event) => {
+  e.target.classList.add("my-colour-class");
+});
