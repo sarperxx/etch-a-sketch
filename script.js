8f51d8e0011fdead9116d85df6882c2f3de3604e
sarperxx <43006015+sarperxx@users.noreply.github.com>
2023-07-25 17:54:59 +0300

    added boxes

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.querySelectorAll(".cell");
 
-//Default grid
-
 // Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
   // Creates rows
@@ -20,12 +18,19 @@ function makeColumns(cellNum) {
     for (j = 0; j < cellNum; j++) {
       let newCell = document.createElement("div");
       rows[j].appendChild(newCell).className = "cell";
+      newCell.style.height = "100%";
+      let height = 5000 / (parseInt(cellNum) + parseInt(cellNum));
+      newCell.style.height = `${height}px`;
+      newCell.style.width = `${height}px`;
     }
   }
 }
+//Default grid
 makeRows(16);
 makeColumns(16);
-
+//change color
 addEventListener("mouseover", (event) => {
-  e.target.classList.add("my-colour-class");
+  if (event.target.className === "cell") {
+    event.target.classList.add("my-colour-class");
+  }
 });
