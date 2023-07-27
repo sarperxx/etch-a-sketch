48288c159da6969159681e6bbef807026f24aaff
sarperxx <43006015+sarperxx@users.noreply.github.com>
2023-07-27 14:52:54 +0300

    project has been finished

--- script.js
+++ script.js
@@ -19,18 +19,52 @@ function makeColumns(cellNum) {
       let newCell = document.createElement("div");
       rows[j].appendChild(newCell).className = "cell";
       newCell.style.height = "100%";
-      let height = 5000 / (parseInt(cellNum) + parseInt(cellNum));
+      let height = 1400 / (parseInt(cellNum) + parseInt(cellNum));
       newCell.style.height = `${height}px`;
       newCell.style.width = `${height}px`;
     }
   }
 }
+//create rgb colors
+const createRandomRGB = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+};
 //Default grid
 makeRows(16);
 makeColumns(16);
-//change color
-addEventListener("mouseover", (event) => {
-  if (event.target.className === "cell") {
-    event.target.classList.add("my-colour-class");
+//change to rainbow
+function changerainbow() {
+  addEventListener("mouseover", (event) => {
+    if (event.target.className === "cell") {
+      event.target.style.backgroundColor = createRandomRGB();
+    }
+  });
+}
+
+//change to black
+function changeblack() {
+  addEventListener("mouseover", (event) => {
+    if (event.target.className === "cell") {
+      event.target.style.backgroundColor = "black";
+    }
+  });
+}
+
+let erase = document.getElementById("myBtn");
+const randomColor = document.getElementById("color");
+
+function eraseFunction() {
+  let pixel = prompt("Please enter a number between 1-100");
+  if (pixel > 100) {
+    alert("You cannot enter a number bigger than 100");
+    return;
   }
-});
+  container.innerHTML = "";
+  makeRows(pixel);
+  makeColumns(pixel);
+}
+
+erase.addEventListener("click", eraseFunction);
